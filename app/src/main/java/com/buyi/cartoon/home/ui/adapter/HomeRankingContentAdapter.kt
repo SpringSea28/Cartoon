c4bbfb061dc8b0400a503786bf5c96feda178c2c
chunhai <wchwch28@163.com>
2023-12-03 21:36:41 +0800

    1、排名

--- app/src/main/java/com/buyi/cartoon/home/ui/adapter/HomeRankingContentAdapter.kt
+++ app/src/main/java/com/buyi/cartoon/home/ui/adapter/HomeRankingContentAdapter.kt
@@ -2,8 +2,13 @@ package com.buyi.cartoon.home.ui.adapter
 
 import android.annotation.SuppressLint
 import android.content.ClipData.Item
+import android.content.Context
+import android.util.TypedValue
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
@@ -11,6 +16,7 @@ import com.buyi.cartoon.R
 import com.buyi.cartoon.databinding.HomeClassifyContentItemBinding
 import com.buyi.cartoon.databinding.HomeRankingContentItemBinding
 import com.buyi.cartoon.http.bean.DemoReqData
+import com.buyi.cartoon.main.utils.Tools
 
 class HomeRankingContentAdapter:PagingDataAdapter<DemoReqData.DataBean.DatasBean,
         HomeRankingContentAdapter.ItemVh>(object :
@@ -50,6 +56,10 @@ class HomeRankingContentAdapter:PagingDataAdapter<DemoReqData.DataBean.DatasBean
             holder.binding.tvRankingNumber.setBackgroundResource(R.drawable.bg_9_4d000000)
             holder.binding.tvRankingNumber.setTextColor(context.getColor(R.color.main_color_pink))
         }
+        val text1 = datasBean?.author
+        addTextView(context,text1,holder.binding.llLabel)
+        val text2 = datasBean?.chapterName
+        addTextView(context,text2,holder.binding.llLabel)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemVh {
@@ -59,5 +69,20 @@ class HomeRankingContentAdapter:PagingDataAdapter<DemoReqData.DataBean.DatasBean
 
     class ItemVh(val binding : HomeRankingContentItemBinding) : RecyclerView.ViewHolder(binding.root){
 
+    }
+
+    private fun addTextView(context:Context,text:String?,linearLayout: LinearLayout){
+        val label1 = TextView(context)
+        val layoutParam = LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+            ViewGroup.LayoutParams.MATCH_PARENT)
+        layoutParam.marginEnd = Tools.dip2px(context,8.0f)
+        val padding = Tools.dip2px(context,7.0f)
+        label1.setPadding(padding,0,padding,0)
+        label1.setTextColor(context.getColor(R.color.home_classify_nav_tab_text_inactive_col))
+        label1.background = context.getDrawable(R.drawable.bg_9_stroke_fb709c)
+        label1.gravity = Gravity.CENTER
+        label1.text = text
+        label1.setTextSize(TypedValue.COMPLEX_UNIT_SP,12f)
+        linearLayout.addView(label1,layoutParam)
     }
 }
---
+++ app/src/main/java/com/buyi/cartoon/main/utils/Tools.kt
@@ -0,0 +1,24 @@
+package com.buyi.cartoon.main.utils
+
+import android.content.Context
+
+
+
+
+object Tools {
+
+    // 根据手机的分辨率从 dp 的单位 转成为 px(像素)
+    fun dip2px(context: Context, dpValue: Float): Int {
+        // 获取当前手机的像素密度（1个dp对应几个px）
+        val scale = context.resources.displayMetrics.density
+        return (dpValue * scale + 0.5f).toInt() // 四舍五入取整
+    }
+
+    // 根据手机的分辨率从 px(像素) 的单位 转成为 dp
+    fun px2dip(context: Context, pxValue: Float): Int {
+        // 获取当前手机的像素密度（1个dp对应几个px）
+        val scale = context.resources.displayMetrics.density
+        return (pxValue / scale + 0.5f).toInt() // 四舍五入取整
+    }
+
+}
